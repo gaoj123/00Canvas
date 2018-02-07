c1946d0f48a0656a574eed5be53e938c2a1e4630
Jenny Gao <jgao658@gmail.com>
2018-02-07 10:56:04 -0500

    can now connect dots

--- canvas.js
+++ canvas.js
@@ -1,47 +1,41 @@
-var rect=true;
 var clearButton=document.getElementById("clear")
-var toggleButton=document.getElementById("toggle")
-
 var canvas=document.getElementById("slate");
 var ctx=canvas.getContext("2d");
-var recta=function(e){
-    mouseX=e.pageX;
-    mouseY=e.pageY;
-    ctx.beginPath();
-    ctx.fillStyle="red";
-    ctx.fillRect(mouseX-10,mouseY-10,20,20);
-};
+
+var started=false;
 
 var circle=function(e){
-    mouseX=e.pageX;
-    mouseY=e.pageY;
+    mouseX=e.offsetX;
+    mouseY=e.offsetY;
+    started=true;
     ctx.beginPath();
     ctx.arc(mouseX,mouseY,10,0,2*Math.PI);
     ctx.fillStyle="blue";
     ctx.fill();
     ctx.stroke();
+    ctx.moveTo(mouseX,mouseY);
 };
 
+var clear=function(e){
+    ctx.clearRect(0,0,500,500);
+    ctx.beginPath();
+}
+
 var draw=function(e){
-    if (rect){
-	recta(e);
+    if (started){
+	mouseX=e.offsetX;
+	mouseY=e.offsetY;
+	ctx.lineTo(mouseX,mouseY);
+	ctx.stroke();
+	circle(e);
     }
     else{
 	circle(e);
     }
 };
-
-var clear=function(e){
-    ctx.clearRect(0,0,500,500);
-}
-
-var toggle=function(e){
-    rect=!rect;
-}
-
     
 canvas.addEventListener("click",draw);
 clearButton.addEventListener("click", clear);
-toggleButton.addEventListener("click", toggle);
+
 
 
